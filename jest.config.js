0f8f2f35e1b993258fb2ea193cf41ae182669ebe
Jon Sakas <jon.sakas@gmail.com>
2019-07-01 00:12:45 -0600

    Fix jest config lint

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    "verbose": true,
-    "collectCoverage": true,
-    "collectCoverageFrom": [
-        "lib/**/*.js",
-        "!lib/implementedProperties.js",
-        "!lib/properties.js",
-    ],
-    "coverageDirectory": "coverage",
+  "verbose": true,
+  "collectCoverage": true,
+  "collectCoverageFrom": [
+    "lib/**/*.js",
+    "!lib/implementedProperties.js",
+    "!lib/properties.js",
+  ],
+  "coverageDirectory": "coverage",
 };
