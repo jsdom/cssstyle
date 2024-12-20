8a19acdcb40c6fd856b025b3b14edd47a46ab65d
asamuzaK (Kazz) <asamuzaK@users.noreply.github.com>
2024-12-20 13:34:22 +0900

    Extend CSS color support with css-color module
    Fixes #134. Fixes #135. Fixes #136. Fixes #174. Closes #172.

--- test/parsers.js
+++ test/parsers.js
@@ -114,13 +114,64 @@ describe('parseColor', () => {
   it('should convert not zero hsl with non zero hue 120 to rgb(0, 255, 0)', () => {
     let input = 'hsl(120, 100%, 50%)';
     let output = parsers.parseColor(input);
+
     assert.strictEqual(output, 'rgb(0, 255, 0)');
   });
 
   it('should convert not zero hsl with non zero hue 240 to rgb(0, 0, 255)', () => {
     let input = 'hsl(240, 100%, 50%)';
     let output = parsers.parseColor(input);
+
     assert.strictEqual(output, 'rgb(0, 0, 255)');
+  });
+
+  it('should convert modern rgb to rgb values', () => {
+    let input = 'rgb(128 0 128 / 1)';
+    let output = parsers.parseColor(input);
+
+    assert.strictEqual(output, 'rgb(128, 0, 128)');
+  });
+
+  it('should convert modern rgb with none values to rgb values', () => {
+    let input = 'rgb(128 0 none)';
+    let output = parsers.parseColor(input);
+
+    assert.strictEqual(output, 'rgb(128, 0, 0)');
+  });
+
+  it('should convert modern rgba to rgba values', () => {
+    let input = 'rgba(127.5 0 127.5 / .5)';
+    let output = parsers.parseColor(input);
+
+    assert.strictEqual(output, 'rgba(128, 0, 128, 0.5)');
+  });
+
+  it('should normalize lab values', () => {
+    let input = 'lab(46.2775% -47.5621 48.5837 / 1.0)'; // green
+    let output = parsers.parseColor(input);
+
+    assert.strictEqual(output, 'lab(46.2775 -47.5621 48.5837)');
+  });
+
+  it('should normalize color function values', () => {
+    let input = 'color(srgb 0 .5 0 / 1.0)';
+    let output = parsers.parseColor(input);
+
+    assert.strictEqual(output, 'color(srgb 0 0.5 0)');
+  });
+
+  it('should normalize color-mix values', () => {
+    let input = 'color-mix(in srgb, rgb(255 0 0), #0000ff 40%)';
+    let output = parsers.parseColor(input);
+
+    assert.strictEqual(output, 'color-mix(in srgb, rgb(255, 0, 0) 60%, rgb(0, 0, 255))');
+  });
+
+  it('should not remove comments, trim or lower case letters if var() is used', () => {
+    let input = 'var( --custom-Color /* comment */)';
+    let output = parsers.parseColor(input);
+
+    assert.strictEqual(output, 'var( --custom-Color /* comment */)');
   });
 
   it.todo('Add more tests');
