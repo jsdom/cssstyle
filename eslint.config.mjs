d89caecb6fd9d683dd29ce914b5db4ace8bd5cb3
asamuzaK (Kazz) <kazz@asamuzak.jp>
2025-05-13 05:59:05 +0900

    Get rid of `var`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -23,16 +23,6 @@ export default [
     }
   },
   {
-    // FIXME:
-    // see https://github.com/jsdom/cssstyle/issues/201
-    // see https://github.com/jsdom/cssstyle/issues/202
-    files: ["lib/parsers.js", "lib/properties/*.js"],
-    rules: {
-      "no-invalid-this": "off",
-      "no-var": "off"
-    }
-  },
-  {
     files: ["scripts/**/*"],
     languageOptions: {
       globals: globals.node
