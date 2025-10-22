48edfe7458f90b275ac2d0277241d11c67c047c4
Acemir Sousa Mendes <acemirsm@gmail.com>
2025-10-22 03:15:10 +0100

    test: sync with jsdom tuwpt

--- test/CSSStyleDeclaration.test.js
+++ test/CSSStyleDeclaration.test.js
@@ -169,17 +169,42 @@ describe("CSSStyleDeclaration", () => {
     const style = new CSSStyleDeclaration(null, {
       context: node
     });
-    style.cssText = "color: olivedrab; &.d { color: peru; }";
-    assert.strictEqual(style.cssText, "color: olivedrab;");
-    style.cssText = "color: blue; @media screen { color: red; } color: orange;";
-    assert.strictEqual(style.cssText, "color: orange;");
+
+    // valid property followed by invalid property followed by valid property
     style.cssText = "color: green; color: invalid!; background: blue;";
+    // ignores invalid properties
     assert.strictEqual(style.cssText, "color: green; background: blue;");
+
+    // only valid properties
+    style.cssText = "color: olivedrab; color: peru; background: bisque;";
+    // keeps the last one of the same property
+    assert.strictEqual(style.cssText, "color: peru; background: bisque;");
+
+    // valid property followed by a nested selector rule
+    style.cssText = "color: olivedrab; &.d { color: peru; }";
+    // ignores the nested selector rule
+    assert.strictEqual(style.cssText, "color: olivedrab;");
+
+    // valid property followed by a nested selector rule followed by two valid properties and an invalid property
     style.cssText =
       "color: olivedrab; &.d { color: peru; } color: green; background: red; invalid: rule;";
+    // ignores the property immediately after the nested rule
     assert.strictEqual(style.cssText, "color: olivedrab; background: red;");
-    style.cssText =
-      "color: blue; &.d { color: peru; } @media screen { color: red; } @layer { color: black; } color: pink; background: orange;";
+
+    // valid property followed by a at-rule followed by a valid property
+    style.cssText = "color: blue; @media screen { color: red; } color: orange;";
+    // includes the the property immediately after an at-rule
+    assert.strictEqual(style.cssText, "color: orange;");
+
+    // valid property followed by a nested rule, two at-rules and two valid properties
+    style.cssText = `
+      color: blue;
+      &.d { color: peru; }
+      @media screen { color: red; }
+      @layer { color: black; }
+      color: pink;
+      background: orange;`;
+    // ignores the first property found after the nested selector rule along with the at-rules
     assert.strictEqual(style.cssText, "color: blue; background: orange;");
   });
 
