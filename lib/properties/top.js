jsdom/cssstyle

commit c06fbabbc6531ad39a29555aab32096d5bfd89cd

Add resolvers and apply to properties/*.js

lib/properties/top.js
@@ -4,7 +4,7 @@ const parsers = require("../parsers");
 
 const property = "top";
 
-module.exports.parse = function parse(v, opt = {}) {
+module.exports.parse = (v, opt = {}) => {
   const { globalObject } = opt;
   if (v === "") {
     return v;
@@ -14,22 +14,10 @@ module.exports.parse = function parse(v, opt = {}) {
     inArray: true
   });
   if (Array.isArray(value) && value.length === 1) {
-    const [{ isNumber, name, type, value: itemValue }] = value;
-    switch (type) {
-      case "Calc": {
-        if (isNumber) {
-          return;
-        }
-        return `${name}(${itemValue})`;
-      }
-      case "GlobalKeyword":
-      case "Identifier": {
-        return name;
-      }
-      default: {
-        return parsers.parseLengthPercentage(value);
-      }
-    }
+    return parsers.resolveNumericValue(value, {
+      min: 0,
+      type: "length"
+    });
   } else if (typeof value === "string") {
     return value;
   }

commit 235ba62cbf5280ddd441e842fad56a86c576260b

Fix top property always clamped to 0
This regressed in c06fbabbc6531ad39a29555aab32096d5bfd89cd. Fixes #281. Test: https://github.com/jsdom/jsdom/pull/4012.

lib/properties/top.js
@@ -15,7 +15,6 @@ module.exports.parse = (v, opt = {}) => {
   });
   if (Array.isArray(value) && value.length === 1) {
     return parsers.resolveNumericValue(value, {
-      min: 0,
       type: "length"
     });
   } else if (typeof value === "string") {